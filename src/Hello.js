9fca00495a41b02ae1a570c111a3cd2063440abd
Mateusz Derks <mateusz.derks@toptal.com>
2019-05-15 17:05:59 +0200

    Rework test without suspense

--- src/Hello.js
+++ src/Hello.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 import { useQuery } from 'react-apollo-hooks';
 
 export default function Hello() {
-  const { data, error } = useQuery(gql`
+  const { data, error, loading } = useQuery(gql`
     query HelloQuery {
       hello
     }
@@ -11,6 +11,10 @@ export default function Hello() {
 
   if (error) {
     throw error;
+  }
+
+  if (loading) {
+    return <div>Loading</div>;
   }
 
   return <div>{data.hello}</div>;
